96e6cf1c53a61340db2998ab5c2c5a9a23580321
Eventrific <eventrific@gmail.com>
2020-04-12 01:44:54 +0700

    change default path services to service

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
 const basePath: string = path.resolve(process.cwd())
-const servicePath: string = path.resolve(basePath, (process.env.SERVICE_PATH || 'services'))
+const servicePath: string = path.resolve(basePath, (process.env.SERVICE_PATH || 'service'))
 const routePath: string = path.resolve(basePath, (process.env.ROUTER || 'router.json'))
 const logPath: string = path.resolve(basePath, (process.env.LOG_PATH || 'log'))
 const middlewarePath: string = path.resolve(basePath, (process.env.MIDDLEWARE_PATH || 'middleware'))
