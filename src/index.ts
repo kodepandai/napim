c6ddfcdb7472106a54def9043873be7d69d074ed
Akhmad Salafudin <axmad386@gmail.com>
2022-12-01 09:35:07 +0700

    feat: add MAX_JSON env to customize json parser limit

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ dotenv.config()
 import polka, { Polka } from 'polka'
 import router from "./router";
 import Parser from 'body-parser'
-const {json} = Parser
-import {ApiCall,
+const { json } = Parser
+import {
+  ApiCall,
   registerDb,
   handleError,
   parseError,
@@ -18,19 +19,20 @@ import {ApiCall,
   Log,
   extendRule,
   addCustomMessages,
-  extendMessages} from "./core/ServiceProvider";
+  extendMessages
+} from "./core/ServiceProvider";
 import { basePath, servicePath, logPath, routePath, middlewarePath } from './utils/path'
 const port: string | number = process.env.PORT || 3000;
 const app = polka();
 /**
  * Start Node API Maker
  */
-const start = (config: { db?: any, beforeStart?: any, listen?:boolean} = { db: null, beforeStart: () => { }, listen:true}):Polka['handler']|Polka => {
+const start = (config: { db?: any, beforeStart?: any, listen?: boolean } = { db: null, beforeStart: () => { }, listen: true }): Polka['handler'] | Polka => {
   const listen = config.listen ?? true
   try {
     Console.info('Starting framework...')
     registerDb(config.db, config.beforeStart)
-    app.use(json())
+    app.use(json({ limit: process.env.MAX_JSON || '50mb' }))
     app.use(router);
     if (!listen) return app.handler
     return app.listen(port, () => onListening(port));
@@ -54,7 +56,8 @@ const onListening = (port: string | number) => {
 
 export type { IMiddleware, IErrorData, IGmInstance, IKeyVal, IRoute, IRoutes, IService, ReqExtended } from "./utils/interface";
 export { app, start, router }
-export {ApiCall,
+export {
+  ApiCall,
   registerDb,
   handleError,
   parseError,
@@ -68,5 +71,6 @@ export {ApiCall,
   Log,
   extendRule,
   addCustomMessages,
-  extendMessages}
+  extendMessages
+}
 export { basePath, servicePath, logPath, routePath, middlewarePath }
